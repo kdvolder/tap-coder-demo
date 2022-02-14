604693e567b151d006a0e7e39947b2cc41c340f5
Kris De Volder <kdevolder@pivotal.io>
2022-02-14 13:12:48 -0800

    Try once more to configure aws / eksctl

--- personalize.sh
+++ personalize.sh
@@ -12,44 +12,36 @@ fi
 
 echo "Cloudgate Client ID:     $CG_CLIENT_ID"
 
-CG_API_TOKEN=$(curl -s --location --request POST 'https://api.console.cloudgate.vmware.com/authn/token' \
+export CG_API_TOKEN=$(curl -s --location --request POST 'https://api.console.cloudgate.vmware.com/authn/token' \
   --user "${CG_CLIENT_ID}:${CG_CLIENT_SECRET}" \
   --header 'Content-Type: application/json' \
   --data-raw '{ "grant_type": "client_credentials" }' | jq -r .access_token)
 
-echo "CG API Token: $CG_API_TOKEN"
-
-CG_ACCESS=$(curl -s 'https://api.console.cloudgate.vmware.com/access/access' \
+curl -s 'https://api.console.cloudgate.vmware.com/access/access' \
   -H 'accept: application/json' \
   -H 'content-type: application/json' \
   -H "authorization: Bearer ${CG_API_TOKEN}" \
-  --data-raw '{"ouId":"ou-kw69-lqh1erao","orgAccountId":"002159642820","role":"PowerUser","masterAccountId":"116462199383"}')
+  --data-raw '{"ouId":"ou-kw69-lqh1erao","orgAccountId":"002159642820","role":"PowerUser","masterAccountId":"116462199383"}' > /tmp/cg-access.json
 
-echo "$CG_ACCESS" | jq
-
-AWS_ACCESS_KEY_ID=$(echo "$CG_ACCESS" | jq -r ".credentials.accessKeyId")
-AWS_SECRET_KEY_ID=$(echo "$CG_ACCESS" | jq -r ".credentials.secretAccessKey")
-AWS_SESSION_TOKEN=$(echo "$CG_ACCESS" | jq -r ".credentials.sessionToken")
-
-echo "AWS Access Key ID: $AWS_ACCESS_KEY_ID"
-echo "AWS Secret Key ID: $AWS_SECRET_KEY_ID"
-echo "AWS Session Token: $AWS_SESSION_TOKEN"
+AWS_ACCESS_KEY_ID=$(jq -r .credentials.accessKeyId /tmp/cg-access.json)
+AWS_SECRET_ACCESS_KEY=$(jq -r .credentials.secretAccessKey /tmp/cg-access.json)
+AWS_SESSION_TOKEN=$(jq -r .credentials.sessionToken /tmp/cg-access.json)
 
 mkdir -p ~/.aws
-cd ~/.aws
-
-echo >~/.aws/credentials <<EOF
+envsubst >  ~/.aws/credentials << EOF
 [default]
 aws_access_key_id=${AWS_ACCESS_KEY_ID}
-aws_secret_access_key=${AWS_SECRET_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
 aws_session_token=${AWS_SESSION_TOKEN}
 EOF
 
-echo >~/.aws/config <<EOF
-[default]
-region=us-west-1
-output=json
-EOF
+# echo >~/.aws/config <<EOF
+# [default]
+# region=us-west-1
+# output=json
+# EOF
+
+eksctl get cluster --region us-west-1
 
 echo "---------------------------------------------"
 echo "---- END tap-coder-demo-personalizations ----"
